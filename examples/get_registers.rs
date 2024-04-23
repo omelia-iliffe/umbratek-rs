003d7c869d351ed63f122588352ec35bbc496a08
Omelia <omelia.iliffe@wetaworkshop.co.nz>
2024-04-23 15:51:58 +1200

    rename to umbratek, updated examples

--- examples/get_registers.rs
+++ examples/get_registers.rs
@@ -1,55 +1,57 @@
-use umbratek_rs::protocol::registers;
-use umbratek_rs::{Bus, ReadError, TransferError};
-
-const MOTOR_ID: u8 = 1;
+use umbratek::{Bus, ReadError, TransferError};
+use umbratek::protocol::registers;
 
 fn main() -> Result<(), TransferError> {
-	env_logger::Builder::from_default_env().init();
-	let mut bus = Bus::open_with_buffers("/dev/ttyUSB0", 4000000, vec![0; 1024], vec![0; 1024]).map_err(ReadError::from)?;
+    env_logger::Builder::from_default_env().init();
+    let motor_id = std::env::var("MOTOR_ID").map_or(1, |v| v.parse::<u8>().unwrap_or(1));
 
-	// print every readable register
-	println!("{:?}", bus.read::<registers::UUID>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::SWVersion>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::HWVersion>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::MultiVersion>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::MechRatio>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::ElecRatio>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::IWDGCyc>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TempLimit>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VoltLimit>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::CurrLimit>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::MotionMode>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::MotionEnable>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::BrakeEnable>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TempDriver>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TempMotor>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::BusVolt>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::BusCurr>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::BattVolt>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::ErrorCode>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::PositionTarget>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::PositionCurrent>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::PositionLimitMax>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::PositionLimitMin>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::PositionLimitDiff>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::PositionPIDP>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::PositionSmoothCyc>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocityTarget>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocityCurrent>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocityLimitMax>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocityLimitMin>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocityLimitDiff>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocityPIDP>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocityPIDI>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::VelocitySmoothCyc>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorqueTarget>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorqueCurrent>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorqueLimitMax>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorqueLimitMin>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorqueLimitDiff>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorquePIDP>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorquePIDI>(MOTOR_ID)?.data);
-	println!("{:?}", bus.read::<registers::TorqueSmoothCyc>(MOTOR_ID)?.data);
+    let baud_rate = std::env::var("BAUD_RATE").map_or(921600, |v| v.parse::<u32>().unwrap_or(921600));
+    let mut bus = Bus::open_with_buffers("/dev/ttyUSB0", baud_rate, vec![0; 1024], vec![0; 1024]).map_err(ReadError::from)?;
 
-	Ok(())
+
+    // print every readable register
+    println!("{:?}", bus.read::<registers::UUID>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::SWVersion>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::HWVersion>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::MultiVersion>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::MechRatio>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::ElecRatio>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::IWDGCyc>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TempLimit>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VoltLimit>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::CurrLimit>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::MotionMode>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::MotionEnable>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::BrakeEnable>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TempDriver>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TempMotor>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::BusVolt>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::BusCurr>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::BattVolt>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::ErrorCode>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::PositionTarget>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::PositionCurrent>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::PositionLimitMax>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::PositionLimitMin>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::PositionLimitDiff>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::PositionPIDP>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::PositionSmoothCyc>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocityTarget>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocityCurrent>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocityLimitMax>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocityLimitMin>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocityLimitDiff>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocityPIDP>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocityPIDI>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::VelocitySmoothCyc>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorqueTarget>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorqueCurrent>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorqueLimitMax>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorqueLimitMin>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorqueLimitDiff>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorquePIDP>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorquePIDI>(motor_id)?.data);
+    println!("{:?}", bus.read::<registers::TorqueSmoothCyc>(motor_id)?.data);
+
+    Ok(())
 }
